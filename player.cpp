74f0de312efd83b58bf5240630275308282da457
user <onisimpostica@gmail.com>
2025-10-19 13:18:59 +0300

    update

--- player.cpp
+++ player.cpp
@@ -2,6 +2,65 @@
 #include <iostream>
 #include <limits>
 
+Player::Player() : name(""), choice(Choice::Rock) {}
+
+Player::Player(const std::string& name, Choice choice) : name(name), choice(choice) {}
+
+Player::Player(const Player& other) : name(other.name), choice(other.choice) {}
+
+Player& Player::operator=(const Player& other) {
+    if (this != &other) {
+        name = other.name;
+        choice = other.choice;
+    }
+    return *this;
+}
+
+bool Player::operator==(const Player& other) const {
+    return name == other.name && choice == other.choice;
+}
+
+bool Player::operator!=(const Player& other) const {
+    return !(*this == other);
+}
+
+bool Player::operator<(const Player& other) const {
+    return name < other.name || (name == other.name && static_cast<int>(choice) < static_cast<int>(other.choice));
+}
+
+bool Player::operator>(const Player& other) const {
+    return other < *this;
+}
+
+std::istream& operator>>(std::istream& is, Player& player) {
+    std::cout << "Enter player name: ";
+    is >> player.name;
+    int input;
+    do {
+        std::cout << "Enter 0 (Rock), 1 (Paper), or 2 (Scissors): ";
+        is >> input;
+        if (is.fail() || !(input >= 0 && input <= 2)) {
+            is.clear();
+            is.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Invalid input. Please try again." << std::endl;
+        } else {
+            player.choice = static_cast<Choice>(input);
+            break;
+        }
+    } while (true);
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const Player& player) {
+    os << "Player: " << player.name << ", Choice: ";
+    switch (player.choice) {
+        case Choice::Rock: os << "Rock"; break;
+        case Choice::Paper: os << "Paper"; break;
+        case Choice::Scissors: os << "Scissors"; break;
+    }
+    return os;
+}
+
 void Player::makeChoice() {
     int input;
     do {
--- player.hpp
+++ player.hpp
@@ -1,14 +1,31 @@
 #pragma once
 
+#pragma once
 #include <iostream>
-#include <limits>
-#include "choice.hpp"
+
+enum class Choice { Rock = 0, Paper = 1, Scissors = 2 };
 
 class Player {
-private:
+public:
+    std::string name;
     Choice choice;
 
-public:
+    Player();
+
+    Player(const std::string& name, Choice choice);
+
+    Player(const Player& other);
+
+    Player& operator=(const Player& other);
+
+    bool operator==(const Player& other) const;
+    bool operator!=(const Player& other) const;
+    bool operator<(const Player& other) const;
+    bool operator>(const Player& other) const;
+
+    friend std::istream& operator>>(std::istream& is, Player& player);
+    friend std::ostream& operator<<(std::ostream& os, const Player& player);
+
     void makeChoice();
     Choice getChoice() const;
 };
